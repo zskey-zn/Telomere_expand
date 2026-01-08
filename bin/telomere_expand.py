ec3320498480247c4593f10ecab5b9187f6d0c6c
zheng shang <55039417+zskey-zn@users.noreply.github.com>
2026-01-08 11:01:42 +0800

    Update telomere_expand.py

--- bin/telomere_expand.py
+++ bin/telomere_expand.py
@@ -3,7 +3,7 @@
 Fix on Mon Dec 30 11:49:35 CST 2024
 telomere expand
 
-@author: zhengshang@frasergen.com   2398848440@qq.com
+@author: zhengshang@frasergen.com zhengshang-zn@qq.com
 
 """
 import os
@@ -14,21 +14,17 @@ parser = argparse.ArgumentParser(description="telomere expand")
 parser.add_argument('--info',help='expand infomation path',required=True)
 parser.add_argument('--genome',help='gap-free genome path',required=True)
 parser.add_argument('--candidate',help='candidate contig path',required=True)
+parser.add_argument('--category',help='species category',default=True)
 parser.add_argument('--map_para',help='minimap2 parameter',default="map-hifi")
 argv=vars(parser.parse_args())
 info_file=argv['info'].strip()
 genome_file=argv['genome'].strip()
 candidate_file=argv['candidate'].strip()
 map_para=argv['map_para'].strip()
-script_path=os.path.dirname(os.path.realpath(__file__)) + "/software.txt"
-f=open(script_path,'r')
-data=f.readlines()
-f.close()
-data=[i[:-1] for i in data if i[0]!="#" and i!="\n"]
-s_a=[i.split('=')[0].strip() for i in data]
-s_b=[i.split('=')[1].strip() for i in data]
-script_dict=dict(zip(s_a,s_b))
-
+category=argv['category'].strip()
+#info_file = "expand.info"
+#genome_file = "../00.gapcloser_merge/gap_free.fa"
+#candidate_file = "../00.gapcloser_merge/candidate_contig.fa"
 info=[]
 with open(info_file) as obj:
     for line in obj:
@@ -45,8 +41,13 @@ with open(info_file) as obj:
             pos_type='head'
         match_pos=int(tmp[3])
         info.append([chr_id,pos_type,contig_id,phase,match_pos])
-        
+
 expand_chr_list=list(set(i[0] for i in info))
+if category == "f":
+    min_match=50
+else:
+    min_match=200
+
 for cell in expand_chr_list:
     cell_content=[i for i in info if i[0]==cell]
     os.system("mkdir {}".format(cell))
@@ -56,33 +57,32 @@ for cell in expand_chr_list:
     f=open("work.sh",'w')
     content="""#!/bin/bash
 echo begin at `date`
-seqkit={}
+module load seqkit
 echo ">{}" > telomere_without.fa
 touch candicate.fa;rm candicate.fa;
-""".format(script_dict['seqkit'],cell)
+""".format(cell)
     #head
     if len(head_content)!=0:
-        content+="""$seqkit faidx {0} {1}:1-{2} | $seqkit seq -w {3} | head -2 | tail -1 >>  telomere_without.fa
+        content+="""seqkit faidx {0} {1}:1-{2} | seqkit seq -w {3} | head -2 | tail -1 >>  telomere_without.fa
 echo {4} >> telomere_without.fa
-$seqkit faidx {0} {1}:1-{5} >> candicate.fa
+seqkit faidx {0} {1}:1-{5} >> candicate.fa
 """.format(candidate_file,head_content[0][2],head_content[0][4],int(head_content[0][4]/100-1)*100,"N"*100,int(head_content[0][4])+20000)
     #gap-free chr
     content+="""echo {} > chr.id
-$seqkit grep -f chr.id {} | $seqkit seq -w 0 | tail -1 >> telomere_without.fa
+seqkit grep -f chr.id {} | seqkit seq -w 0 | tail -1 >> telomere_without.fa
 """.format(cell,genome_file)
     #tail
     if len(tail_content)!=0:
         content+="""echo {0} >> telomere_without.fa
-$seqkit faidx {1} {2}:1-{3} | $seqkit seq -w {4} | head -2 |$seqkit seq -r -p -w 0 | tail -1 >>  telomere_without.fa
-$seqkit faidx {1} {2}:1-{5} >> candicate.fa
+seqkit faidx {1} {2}:1-{3} | seqkit seq -w {4} | head -2 |seqkit seq -r -p -w 0 | tail -1 >>  telomere_without.fa
+seqkit faidx {1} {2}:1-{5} >> candicate.fa
 """.format("N"*100,candidate_file,tail_content[0][2],tail_content[0][4],int(tail_content[0][4]/100-1)*100,int(tail_content[0][4])+20000)
-    
+
     #gapcloser
-    content+="""{} --scaff telomere_without.fa --reads candicate.fa --minmap_arg '-x {}' --min_idy 0.2 --min_match 200 --thread 20  --output gapclose --ne >pipe.log 2>pipe.err
+    content+="""/public/frasergen/PUB/software/TGS-GapCloser/TGS-GapCloser_v1.0.1/TGS-GapCloser.sh --scaff telomere_without.fa --reads candicate.fa --minmap_arg '-x {}' --min_idy 0.2 --min_match {} --thread 20  --output gapclose --ne >pipe.log 2>pipe.err
 echo end at `date`
 touch work.sh.finish
-""".format(script_path['GapCloser'],map_para)
+""".format(map_para,min_match)
     f.write(content)
     f.close()
     os.chdir("..")
-
